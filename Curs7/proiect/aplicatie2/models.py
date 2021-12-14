d2958aaec27cd1de54c15853c479751540b03d93
IuliaStefan0209 <stefaniulia99@gmail.com>
2021-12-14 18:40:30 +0200

    Proiect Django

--- Curs7/proiect/aplicatie2/models.py
+++ Curs7/proiect/aplicatie2/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Logs(models.Model):
+    objects = None
+    action_choices = (('created', 'created'),
+                      ('updated', 'updated'),
+                      ('refresh', 'refresh'))
+
+
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)             #stergere in cascada
+    action = models.CharField('Action', max_length=10, choices= action_choices)
+    url = models.CharField('URL', max_length=100)
