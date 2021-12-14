7bb0bac69dccc16e9b2a878d1123aa0fd1efe4af
IuliaStefan0209 <stefaniulia99@gmail.com>
2021-12-14 13:30:47 +0200

    Proiect Django

--- Curs7/proiect/aplicatie1/models.py
+++ Curs7/proiect/aplicatie1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+#ce e in paranteza se numeste mostenire
+#Location e denumirea tabelului
+class Location(models.Model):
+    #city e string -> varchar in sql -> CharField in django
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):                             #ne ajuta sa zicem ce vrem sa ne afiseze in mom in care apelam Location
+        return f"{self.city} {self.country}"       #self - argumentul propriu al fct
