54c14d3e9e30c7a6fa4f95a0a03e5bbef0e7da75
IuliaStefan0209 <stefaniulia99@gmail.com>
2021-12-16 01:26:49 +0200

    Proiect django

--- Curs7/proiect/aplicatie1/models.py
+++ Curs7/proiect/aplicatie1/models.py
@@ -8,6 +8,7 @@ class Location(models.Model):
     #city e string -> varchar in sql -> CharField in django
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
+    active = models.IntegerField(default=1)       #daca inregistrarea tb sa fie vizibila sau nu
 
     def __str__(self):                             #ne ajuta sa zicem ce vrem sa ne afiseze in mom in care apelam Location
         return f"{self.city} {self.country}"       #self - argumentul propriu al fct
--- Curs7/proiect/aplicatie1/urls.py
+++ Curs7/proiect/aplicatie1/urls.py
@@ -6,5 +6,6 @@ app_name = 'aplicatie1'
 urlpatterns = [
     path('', views.CreateLocationView.as_view(), name='adaugare'),                    #path-urile secundare ale app
     path('<int:pk>/update/', views.UpdateLocationView.as_view(), name='modificare'),
-    path('list/', views.ListLocationView.as_view(), name='listare')
+    path('list/', views.ListLocationView.as_view(), name='listare'),
+    path('delete/<int:pk>/', views.delete_location, name='remove'),
 ]
